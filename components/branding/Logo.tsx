e7c6f6a8f13ef4f8a00a2bf87e8c2238f451d1ae
Mohammad faisal <faisalmd44@gmail.com>
2026-08-10 06:27:53 +0530

    Auto-crop white edges from logo using scale 1.2 transform

--- components/branding/Logo.tsx
+++ components/branding/Logo.tsx
@@ -26,9 +26,10 @@ export function Logo({
       <Image
         source={require('@/assets/images/icon.png')}
         style={{
-          width: size,
-          height: size,
-          borderRadius: size / 2,
+          width: '100%',
+          height: '100%',
+          // Ye scale effect safed borders ko zoom karke crop kar dega
+          transform: [{ scale: 1.2 }],
         }}
         resizeMode="cover"
       />
@@ -45,6 +46,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'transparent',
-    overflow: 'hidden',
+    overflow: 'hidden', // Bahar nikla hua white hissa yahan se cut ho jayega
   },
 });
